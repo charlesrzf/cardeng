13218aab558dc88a0b3c5819bd0e06486c75e1c2
charlesrzf <98257197+charlesrzf@users.noreply.github.com>
2024-01-26 22:25:54 -0300

    Add files via upload

---
+++ openlink.js
@@ -0,0 +1,26 @@
+function openLink(nlink) {
+    switch (nlink){
+        case 'linkedin':
+            window.open("https://www.linkedin.com/in/charlesrf/");
+            break;
+        case 'whatsapp':
+            window.open("https://wa.me/5531991824329");
+            break
+        case 'instagram':
+            window.open("https://www.instagram.com/ge21geotecnologias/");
+            break
+        case 'site':
+            window.open("https://ge21gt.com.br");
+            break
+        case 'email':
+            location.href ="mailto:charles.freitas@grupoge21.com"+"&subject=Contact";
+            break
+        case 'telefone':
+            location.href="tel:+5531991824329";
+            break
+        case 'endereco':
+            window.open("https://goo.gl/maps/gfypdq7pMnaLWrSw7");
+    }
+
+
+}c
